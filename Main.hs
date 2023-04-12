29e817fd37afd9559c015cdac36d1cde1e0fd859
lux <luca.iacovella@vub.be>
2023-04-12 18:15:47 +0200

    config for profiling

--- Main.hs
+++ Main.hs
@@ -3,14 +3,36 @@ import Models.AdventureGame (AdventureGameConfig(..))
 
 main :: IO ()
 main = 
+    -- runGame AdventureGameConfig {
+    --     seed = 0,
+    --     sight = 5,
+    --     waterCap = 12,
+    --     treasurePct = 50,
+    --     waterPct = 5,
+    --     portalPct = 1,
+    --     lavaSinglePct = 10,
+    --     lavaAdjacentPct = 50,
+    --     gridDim = (30, 20)
+    -- }
+    -- runGame AdventureGameConfig {
+    --     seed = 2,
+    --     sight = 5,
+    --     waterCap = 120,
+    --     treasurePct = 50,
+    --     waterPct = 1,
+    --     portalPct = 0.01,
+    --     lavaSinglePct = 30,
+    --     lavaAdjacentPct = 40,
+    --     gridDim = (30, 20)
+    -- }
     runGame AdventureGameConfig {
-        seed = 0,
+        seed = 7,
         sight = 5,
-        waterCap = 12,
+        waterCap = 120,
         treasurePct = 50,
-        waterPct = 5,
-        portalPct = 1,
-        lavaSinglePct = 10,
-        lavaAdjacentPct = 50,
+        waterPct = 1,
+        portalPct = 0.01,
+        lavaSinglePct = 42,
+        lavaAdjacentPct = 40,
         gridDim = (30, 20)
     }
