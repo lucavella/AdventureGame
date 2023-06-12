3c8d703649ba1feb4bf3e29e021be783007bf511
lux <luca.iacovella@vub.be>
2023-06-12 03:23:33 +0200

    added bmps

--- Gui/GuiAdventureGame.hs
+++ Gui/GuiAdventureGame.hs
@@ -10,6 +10,7 @@ module Gui.GuiAdventureGame where
     import Data.List
     import Graphics.Gloss
     import Graphics.Gloss.Data.Picture
+    import Graphics.Gloss.Data.Bitmap
     import Graphics.Gloss.Data.Color
     import Graphics.Gloss.Interface.Environment
     import Graphics.Gloss.Interface.IO.Game
@@ -45,6 +46,7 @@ module Gui.GuiAdventureGame where
 
         renderMap state@AdventureGameState{..} = do
             (w, h) <- getScreenSize
+            gridPicture <- gridPicture grid (w, h - 200)
             return $ pictures [
                 topLeft (w, h) $ pictures [
                     color white . translate 20 (-40) . scale 0.1 0.1 . Text $ "Position: " ++ show pos,
@@ -52,7 +54,7 @@ module Gui.GuiAdventureGame where
                     color white . translate 20 (-80) . scale 0.1 0.1 . Text $ "Treasure: " ++ show (length treasureCollected),
                     color white . translate 20 (-100) . scale 0.1 0.1 . Text $ showDistances state,
                     color white . translate 20 (-120) . scale 0.1 0.1 . Text $ show event,
-                    color white . translate 0 (-200) $ gridPicture grid (w, h - 200) ],
+                    translate 0 (-200) gridPicture ],
                 topRight (w, h) $ pictures [
                     color white . translate (-200) (-40) . scale 0.1 0.1 $ Text "Controls",
                     color white . translate (-200) (-60) . scale 0.1 0.1 $ Text "Movement: W A S D",
@@ -78,9 +80,9 @@ module Gui.GuiAdventureGame where
 
 
     -- show subgrid around player position, given a width and height
-    gridPicture :: Grid -> (Int, Int) -> Picture
+    gridPicture :: Grid -> (Int, Int) -> IO Picture
     gridPicture (Grid (x, y) tile gbc) (w, h) =
-        pictures $ playerPic : (concat . mapWithIndexMatrix gridPictureFun $ tg' ++ (r' : bg'))
+        fmap pictures . sequence $ playerPic : (concat . mapWithIndexMatrix gridPictureFun $ tg' ++ (r' : bg'))
         --Text $ show $ length $ concat $ mapWithIndexMatrix gridPictureFun $ tg' ++ (r' : bg')
         where
             tileSize = 24
@@ -106,26 +108,27 @@ module Gui.GuiAdventureGame where
 
             playerTileX = (fromInteger lX) * tileSize + (tileSize / 2) - playerRadius
             playerTileY = (fromInteger tY) * tileSize + (tileSize / 2) - playerRadius
-            playerPic = translate playerTileX (-playerTileY) . color (makeColorI 110 71 5 255) $ circle playerRadius
+            playerPic = return . translate 0 0 . color (makeColorI 110 71 5 255) $ circle playerRadius
 
             colFun row = lPad ++ sublist (x - lX) (x + rX) row -- show row of tiles
 
             gridPictureFun i j t =
-                translate (fromIntegral j * tileSize) (-fromIntegral i * tileSize) $ tilePicture tileSize t
+                fmap (translate (fromIntegral j * (tileSize + 1)) (-fromIntegral i * (tileSize + 1))) $ tilePicture tileSize t
 
-    tilePicture :: Float -> GridTile -> Picture
+    tilePicture :: Float -> GridTile -> IO Picture
     tilePicture size tile = 
-        color c $ polygon [(x1, y1), (x2, y1), (x2, y2), (x1, y2)]
+        case tile of
+            GridTile _ False -> return . Color k $ polygon [(x1, y1), (x2, y1), (x2, y2), (x1, y2)]
+            GridTile t True -> case t of
+                Desert _ -> loadBMP "Images/desert.bmp"
+                Water -> loadBMP "Images/water.bmp"
+                Lava -> loadBMP "Images/lava.bmp"
+                Portal -> loadBMP "Images/portal.bmp"
+                OutOfBounds -> return . Color g $ polygon [(x1, y1), (x2, y1), (x2, y2), (x1, y2)]
         where
             x1 = 0
             x2 = x1 + size
             y1 = 0
             y2 = y1 - size
-            c = case tile of
-                GridTile _ False -> makeColorI 38 38 38 255
-                GridTile t True -> case t of
-                    Desert _ -> makeColorI 89 88 34 255
-                    Water -> makeColorI 35 55 89 255
-                    Lava -> makeColorI 82 23 21 255
-                    Portal -> makeColorI 66 21 82 255
-                    OutOfBounds -> makeColorI 0 0 0 255
+            k = makeColorI 38 38 38 255
+            g = makeColorI 38 38 38 255
